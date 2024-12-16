44ac3cc9d7305da721f99db5c2d9d1474d5316a3
Willem Vermeer <willem@xlinq.io>
2024-12-16 19:13:22 +0100

    day 16 part 1 refactoring

--- src/main/scala/day16/Day16.scala
+++ src/main/scala/day16/Day16.scala
@@ -39,7 +39,7 @@ object Day16      extends ReadFile {
                    |#S#.............#
                    |#################""".stripMargin
   def main(args: Array[String]): Unit = {
-//    println(solve1(example))
+    println(solve1(example))
 //    println(solve1(example2))
     println(solve1(input))
     // println(solve2(example))
@@ -66,56 +66,46 @@ object Day16      extends ReadFile {
       val currentValue: State = unvisited(nextPoint)
       val up                  = Point(nextPoint.x, nextPoint.y - 1)
       if (unvisited.keys.toSeq.contains(up)) {
-        if (currentValue.direction == Up) {
-          if (currentValue.score + 1 < unvisited(up).score) {
-            unvisited.put(up, unvisited(up).copy(score = currentValue.score + 1, direction = Up))
-          }
-        } else {
-          if (currentValue.score + 1001 < unvisited(up).score) {
-            unvisited.put(up, unvisited(up).copy(score = currentValue.score + 1001, direction = Up))
-          }
+        val nextScore = if (currentValue.direction == Up) currentValue.score + 1 else currentValue.score + 1001
+        if (nextScore < unvisited(up).score) {
+          unvisited.put(up, unvisited(up).copy(score = nextScore, direction = Up, history = currentValue.history :+ up))
         }
       }
       val down                = Point(nextPoint.x, nextPoint.y + 1)
       if (unvisited.keys.toSeq.contains(down)) {
-        if (currentValue.direction == Down) {
-          if (currentValue.score + 1 < unvisited(down).score) {
-            unvisited.put(down, unvisited(down).copy(score = currentValue.score + 1, direction = Down))
-          }
-        } else {
-          if (currentValue.score + 1001 < unvisited(down).score) {
-            unvisited.put(down, unvisited(down).copy(score = currentValue.score + 1001, direction = Down))
-          }
+        val nextScore = if (currentValue.direction == Down) currentValue.score + 1 else currentValue.score + 1001
+        if (nextScore < unvisited(down).score) {
+          unvisited.put(
+            down,
+            unvisited(down).copy(score = nextScore, direction = Down, history = currentValue.history :+ down)
+          )
         }
       }
       val left                = Point(nextPoint.x - 1, nextPoint.y)
       if (unvisited.keys.toSeq.contains(left)) {
-        if (currentValue.direction == Left) {
-          if (currentValue.score + 1 < unvisited(left).score) {
-            unvisited.put(left, unvisited(left).copy(score = currentValue.score + 1, direction = Left))
-          }
-        } else {
-          if (currentValue.score + 1001 < unvisited(left).score) {
-            unvisited.put(left, unvisited(left).copy(score = currentValue.score + 1001, direction = Left))
-          }
+        val nextScore = if (currentValue.direction == Left) currentValue.score + 1 else currentValue.score + 1001
+        if (nextScore < unvisited(left).score) {
+          unvisited.put(
+            left,
+            unvisited(left).copy(score = nextScore, direction = Left, history = currentValue.history :+ left)
+          )
         }
       }
       val right               = Point(nextPoint.x + 1, nextPoint.y)
       if (unvisited.keys.toSeq.contains(right)) {
-        if (currentValue.direction == Right) {
-          if (currentValue.score + 1 < unvisited(right).score) {
-            unvisited.put(right, unvisited(right).copy(score = currentValue.score + 1, direction = Right))
-          }
-        } else {
-          if (currentValue.score + 1001 < unvisited(right).score) {
-            unvisited.put(right, unvisited(right).copy(score = currentValue.score + 1001, direction = Right))
-          }
+        val nextScore = if (currentValue.direction == Right) currentValue.score + 1 else currentValue.score + 1001
+        if (nextScore < unvisited(right).score) {
+          unvisited.put(
+            right,
+            unvisited(right).copy(score = nextScore, direction = Right, history = currentValue.history :+ right)
+          )
         }
       }
       visited.put(nextPoint, unvisited(nextPoint))
       unvisited.remove(nextPoint)
       println(visited.size)
     }
+    println(visited(target).history)
     visited(target).score
   }
   private def read(s: String) = Board(s)
